cf2a35777244e560b5ca09aa2f96eb6692bc8c48
Deepak Reddy <153088563+Deepakreddy1510@users.noreply.github.com>
2026-08-13 16:05:23 +0530

    Add: meeting-schedule - submission-0

---
+++ Data Structures & Algorithms/meeting-schedule/submission-0.cpp
@@ -0,0 +1,32 @@
+/**
+ * Definition of Interval:
+ * class Interval {
+ * public:
+ *     int start, end;
+ *     Interval(int start, int end) {
+ *         this->start = start;
+ *         this->end = end;
+ *     }
+ * }
+ */
+
+class Solution {
+public:
+    bool canAttendMeetings(vector<Interval>& intervals) {
+        sort(intervals.begin(),intervals.end(),[](const Interval& a,const Interval& b){
+            return a.end < b.end;
+        });
+
+        int lastEnd = INT_MIN;
+        for(const auto& interval:intervals){
+            if(interval.start >= lastEnd){
+                lastEnd = interval.end;
+            }
+            else{
+                return false;
+            }
+        }
+
+        return true;
+    }
+};
